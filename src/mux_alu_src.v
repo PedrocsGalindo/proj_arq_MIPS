d7912c0d5abd701555ab77d77f71c6b5c81bddc3
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-03-07 23:43:56 -0300

    multiplexador da ALU e inicio do top_level

---
+++ src/mux_alu_src.v
@@ -0,0 +1,13 @@
+module mux_alu_src(
+    input wire [31:0] reg_data,    // Valor ReadData2
+    input wire [31:0] imm_ext,     // Valor imediato 
+    input wire ALUSrc,             // 0 = registrador, 1 = imediato
+    output wire [31:0] mux_out     
+);
+    always @(*) begin
+    if (ALUSrc)
+        mux_out = imm_ext;
+    else
+        mux_out = reg_data;
+    end
+endmodule
--- src/top_level.v
+++ src/top_level.v
@@ -1,7 +1,9 @@
 `timescale 1ns/1ps  // Define unidade de tempo como 1ns e precisão de 1ps, pode causar problemas sem isso
 
 module top_level (
-    input wire clock,
-    input wire [31:0] nextPC,
-    output reg [31:0] PC
+    input wire clock,       
+    input wire reset,               // Reset do regfile
+    output wire [31:0] PC_out,      // Saída do valor atual do PC
+    output wire [31:0] ULA_out,     // Saída da ULA
+    output wire [31:0] MemData_out  // Saída da Memória de Dados
 );
