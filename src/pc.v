f926b9d2cd8b0747d8a5cceec2c9b73c922032a1
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-03-07 23:30:30 -0300

    não é pra ter intanciação

--- src/pc.v
+++ src/pc.v
@@ -5,13 +5,6 @@ module pc (
     input wire [31:0] nextPC,
     output reg [31:0] PC
 );
-    // Instanciação do módulo PC
-    pc PC_inst (
-        .clock(clock),
-        .nextPC(nextPC),
-        .PC(PC)
-    );
-    
     initial begin
         PC = 0;  // Inicializa PC em 0
     end
